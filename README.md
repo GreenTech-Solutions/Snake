3e809813778bf84ee2d75bdc843f027957c00785
Alex_Green <deadpoolweid@yandex.ru>
2015-08-09 00:36:02 +0300

    - Добавлено меню.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Snake
 There will be my view on the Snake game! ~_~
+
+#v0.0.1 pre alpha
---
+++ Snake/Snake/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Snake
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ContinueGameButton = new System.Windows.Forms.Button();
+            this.StartNewGameButton = new System.Windows.Forms.Button();
+            this.SettingsButton = new System.Windows.Forms.Button();
+            this.AboutButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ContinueGameButton
+            // 
+            this.ContinueGameButton.AutoEllipsis = true;
+            this.ContinueGameButton.Location = new System.Drawing.Point(133, 12);
+            this.ContinueGameButton.Name = "ContinueGameButton";
+            this.ContinueGameButton.Size = new System.Drawing.Size(162, 56);
+            this.ContinueGameButton.TabIndex = 0;
+            this.ContinueGameButton.Text = "Continue";
+            this.ContinueGameButton.UseVisualStyleBackColor = true;
+            // 
+            // StartNewGameButton
+            // 
+            this.StartNewGameButton.Location = new System.Drawing.Point(133, 74);
+            this.StartNewGameButton.Name = "StartNewGameButton";
+            this.StartNewGameButton.Size = new System.Drawing.Size(162, 56);
+            this.StartNewGameButton.TabIndex = 1;
+            this.StartNewGameButton.Text = "New Game";
+            this.StartNewGameButton.UseVisualStyleBackColor = true;
+            // 
+            // SettingsButton
+            // 
+            this.SettingsButton.Location = new System.Drawing.Point(133, 136);
+            this.SettingsButton.Name = "SettingsButton";
+            this.SettingsButton.Size = new System.Drawing.Size(162, 56);
+            this.SettingsButton.TabIndex = 2;
+            this.SettingsButton.Text = "Settings";
+            this.SettingsButton.UseVisualStyleBackColor = true;
+            // 
+            // AboutButton
+            // 
+            this.AboutButton.Location = new System.Drawing.Point(133, 197);
+            this.AboutButton.Name = "AboutButton";
+            this.AboutButton.Size = new System.Drawing.Size(162, 56);
+            this.AboutButton.TabIndex = 3;
+            this.AboutButton.Text = "About";
+            this.AboutButton.UseVisualStyleBackColor = true;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(133, 259);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(162, 56);
+            this.ExitButton.TabIndex = 4;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackgroundImage = global::Snake.Resources.SnakeMain;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(444, 329);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.AboutButton);
+            this.Controls.Add(this.SettingsButton);
+            this.Controls.Add(this.StartNewGameButton);
+            this.Controls.Add(this.ContinueGameButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.HelpButton = true;
+            this.Name = "MainMenu";
+            this.Text = "Snake";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ContinueGameButton;
+        private System.Windows.Forms.Button StartNewGameButton;
+        private System.Windows.Forms.Button SettingsButton;
+        private System.Windows.Forms.Button AboutButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
