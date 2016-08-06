f110493ce86e580e164740e059755bccd0dbf0f8
Alex_Green <deadpoolweid@yandex.ru>
2016-08-06 17:43:17 +0300

    LD v 0.2
    - Добавлена возможность сохранения, загрузки, а также возможности
    окртыть с помощью для ресурсов программы
    - Исправлена генерация типов клеток
    - Режим рисования теперь выбирается на функциональной панели
    - Небольшие исправления в размерах элементов и вёрстке

--- Snake/SnakeLevelDesigner/MainWindow.xaml.cs
+++ Snake/SnakeLevelDesigner/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
 using Microsoft.Win32;
 
 namespace SnakeLevelDesigner
@@ -26,16 +29,33 @@ namespace SnakeLevelDesigner
         public MainWindow()
         {
             InitializeComponent();
+
+            rObstacle.IsChecked = true;
+            bSave.IsEnabled = false;
+
+            string[] args = Environment.GetCommandLineArgs();
+
+            if (args.Length > 1)
+            {
+                OpenFile(args[1]);
+                bSave.IsEnabled = true;
+            }
         }
 
         private int width, height;
 
-        private void Create_Click(object sender, RoutedEventArgs e)
+        private void Create_OnClick(object sender, RoutedEventArgs e)
+        {
+            CreateMap();
+            bSave.IsEnabled = true;
+        }
+
+        private void CreateMap()
         {
             width = Convert.ToInt32(tWidth.Text);
             height = Convert.ToInt32(tHeight.Text);
 
-            Map.Children.RemoveRange(0,Map.Children.Count);
+            Map.Children.RemoveRange(0, Map.Children.Count);
 
             Map.Rows = width;
             Map.Columns = height;
@@ -45,8 +65,9 @@ namespace SnakeLevelDesigner
                 for (int j = 0; j < Map.Columns; j++)
                 {
                     var cell = new Label();
-                    cell.Tag = new Cell(CellType.Empty, j,i);
+                    cell.Tag = new Cell(CellType.Empty, j, i);
                     cell.Content = cell.Tag;
+                    ChangeCellColor(cell);
 
                     var border = new Border();
                     border.BorderThickness = new Thickness(2);
@@ -58,15 +79,49 @@ namespace SnakeLevelDesigner
 
             Map.MouseMove += MapOnMouseMove;
             Map.MouseDown += MapOnMouseDown;
+        }
 
+        private void CreateMap(Cells cells = null)
+        {
+            if (Equals(cells, null))
+            {
+                CreateMap();
+                return;
+            }
+
+            width = cells.width;
+            height = cells.height;
+
+            Map.Children.RemoveRange(0, Map.Children.Count);
+
+            Map.Rows = width;
+            Map.Columns = height;
+
+            foreach (var tag in cells.cells)
+            {
+                var cell = new Label {Tag = tag};
+                cell.Content = cell.Tag;
+
+                ChangeCellColor(cell);
+
+
+                var border = new Border
+                {
+                    BorderThickness = new Thickness(2),
+                    BorderBrush = Brushes.Gray,
+                    Child = cell
+                };
+                Map.Children.Add(border);
+            }
+
+            Map.MouseMove += MapOnMouseMove;
+            Map.MouseDown += MapOnMouseDown;
         }
 
         private void MapOnMouseDown(object sender, MouseButtonEventArgs mouseButtonEventArgs)
         {
             if (mouseButtonEventArgs.LeftButton == MouseButtonState.Pressed)
             {
-                var map = sender as UniformGrid;
-
                 CellClicked();                
             }
         }
@@ -77,6 +132,8 @@ namespace SnakeLevelDesigner
             {
                 CellClicked();
             }
+            mouseEventArgs.Handled = true;
+
         }
 
         private void CellClicked()
@@ -84,44 +141,104 @@ namespace SnakeLevelDesigner
             foreach (var child in Map.Children)
             {
                 var border = child as Border;
-                var mapButton = border.Child as Label;
-                if (mapButton.IsMouseOver)
+                var label = border.Child as Label;
+                if (label.IsMouseOver)
                 {
-                    var cell = mapButton.Tag as Cell;
+                    var cell = label.Tag as Cell;
 
-                    switch (cell.cellType)
+                    if (rEmpty.IsChecked == true)
                     {
-                        case CellType.Empty: 
-                            cell.cellType = CellType.Brick;
-                            mapButton.Background = Brushes.SaddleBrown;
-                            break;
-                        case CellType.Brick:
-                            cell.cellType = CellType.Player;
-                            mapButton.Background = Brushes.OrangeRed;
-                            break;
-                        case CellType.Player:
-                            cell.cellType = CellType.Empty;
-                            mapButton.Background = Brushes.WhiteSmoke;
-                            break;
+                        cell.cellType = CellType.Empty;
                     }
+                    else if (rObstacle.IsChecked == true)
+                    {
+                        cell.cellType = CellType.Brick;
+                    }
+                    else if (rPlayer.IsChecked == true)
+                    {
+                        cell.cellType = CellType.Player;
+                    }
+
+                    ChangeCellColor(label);
+
+                    label.Tag = cell;
+                    label.Content = label.Tag.ToString();
                 }
+            }
+        }
+
+        private void ChangeCellColor(Label label)
+        {
+            var cell = label.Tag as Cell;
+            switch (cell.cellType)
+            {
+                case CellType.Empty:
+                    label.Background = Brushes.WhiteSmoke;
+                    break;
+                case CellType.Brick:
+                    label.Background = Brushes.DarkSlateGray;
+                    break;
+                case CellType.Player:
+                    label.Background = Brushes.YellowGreen;
+                    break;
             }
         }
 
         private void Save_OnClick(object sender, RoutedEventArgs e)
         {
             SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Level files (*.lvl)|*.lvl";
+
             if (sfd.ShowDialog(this) == true)
             {
                 string fileName = sfd.FileName;
 
-                string[] textToSave = new string[height];
+                List<Cell> listCells = new List<Cell>();
 
+                foreach (var child in Map.Children)
+                {
+                    var border = child as Border;
+                    var label = border.Child as Label;
+                    listCells.Add(label.Tag as Cell);
+                }
+
+                var cells = new Cells(listCells,width,height);
+
+                var bf = new BinaryFormatter();
+                using (Stream fs = new FileStream(fileName,FileMode.Create,FileAccess.Write,FileShare.None))
+                {
+                    bf.Serialize(fs,cells);
+                }
 
             }
         }
 
+        private void Open_OnClick(object sender, RoutedEventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Filter = "Level files (*.lvl)|*.lvl";
+            if (ofd.ShowDialog(this) == true)
+            {
+                string fileName = ofd.FileName;
 
+                OpenFile(fileName);
+            }
+
+            bSave.IsEnabled = true;
+        }
+
+        private void OpenFile(string path)
+        {
+            Cells cells;
+
+            var bf = new BinaryFormatter();
+            using (Stream fs = File.OpenRead(path))
+            {
+                cells = bf.Deserialize(fs) as Cells;
+            }
+
+            CreateMap(cells);
+        }
     }
 
     public enum CellType
@@ -131,6 +248,7 @@ namespace SnakeLevelDesigner
         Player
     }
 
+    [Serializable]
     class Cell
     {
         public CellType cellType;
@@ -148,6 +266,22 @@ namespace SnakeLevelDesigner
         public override string ToString()
         {
             return $"{X};{Y};{cellType}";
+        }
+    }
+
+    [Serializable]
+    class Cells
+    {
+        public List<Cell> cells;
+
+        public int width;
+        public int height;
+
+        public Cells(List<Cell> cells, int width, int height)
+        {
+            this.cells = cells;
+            this.width = width;
+            this.height = height;
         }
     }
 }
